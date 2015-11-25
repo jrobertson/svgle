910b2d3d1ea412a49f8cd27a6296894b29261b70
Your Name <you@example.com>
2015-11-25 07:18:52 +0000

    major refactoring: The DOM is now built the Domle gem.

--- lib/svgle.rb
+++ lib/svgle.rb
@@ -2,8 +2,8 @@
 
 # file: svgle.rb
 
-require 'rexle'
-require 'rxfhelper'
+require 'domle'
+
 
 # helpful SVG element references: 
 #
@@ -25,100 +25,10 @@ text {fill: red}
 
 CSS
 
-class Style < Hash
 
-  def initialize(parent, h)
-    @parent = parent
 
-    super().merge! h
-
-  end
+class Svgle < Domle
   
-  def []=(k,v)
-
-    super(k,v)
-    @parent[:style] = self.map{|x| x.join(':') }.join(';')
-    @parent.callback.refresh
-    
-  end
-end
-
-class Attributes
-  
-  attr_reader :callback
-  
-  def initialize(parent: nil)
-    @callback = parent
-  end  
-  
-  def style(parent=nil)
-    
-    @callback ||= parent
-    
-    if @style.nil? then
-
-      h = self[:style].split(';').inject({}) do |r, x|
-        k, v = x.split(':',2)
-        r.merge(k.to_sym => v)
-      end
-
-      @style = Style.new(self, h)
-
-    end
-    @style
-  end
-  
-end
-
-
-class Svgle < Rexle
-  
-  class Element < Rexle::Element
-
-    def initialize(name=nil, value: nil, \
-                          attributes: Attributes.new(parent: self), rexle: nil)
-      
-      attributes.merge!(style: '') unless attributes.has_key? :style
-      super(name, value: value, attributes: attributes, rexle: rexle)
-      
-    end
-    
-    def self.attr2_accessor(*a)
-
-      a.concat %i(id transform)
-      a.concat %i(onmousemove onmousedown) # DOM events      
-      
-      a.each do |attribute|
-
-        class_eval do
-
-          define_method attribute.to_s.gsub('-','_').to_sym do 
-            attributes[attribute]
-          end
-
-          define_method (attribute.to_s + '=').to_sym do |val|
-            attributes[attribute] = val
-            @rexle.refresh
-            val
-          end
-
-        end
-      end
-    end
-    
-    def style()
-      attributes.style(@rexle)
-    end
-    
-    
-    def hotspot?(x,y)
-
-      (boundary.first.is_a?(Array) ? boundary : [boundary]).all? do |box|
-        x1, y1, x2, y2 = box
-        (x1..x2).include? x and (y1..y2).include? y
-      end
-    end    
-  end
   
   class A < Element
     # undecided how to implement this 
@@ -186,16 +96,6 @@ class Svgle < Rexle
     end    
     
   end  
-
-  class Script < Element
-
-  end  
-  
-  class Style < Element
-    
-
-  end  
-  
   
   class Svg < Element
     attr2_accessor *%i(width height background-color)
@@ -210,21 +110,11 @@ class Svgle < Rexle
       [0,0,0,0]
     end    
   end    
-
-
-  def initialize(x=nil, callback: nil)
-    super x    
-    find_add_css()
-    @callback = callback
-  end
   
   def inspect()    
     "#<Svgle:%s>" % [self.object_id]
   end  
   
-  def refresh()
-    @callback.refresh if @callback
-  end
   
   private
   
@@ -253,50 +143,10 @@ class Svgle < Rexle
       
     end
 
-
   end
   
-  def add_css(s)
-
-    # parse the CSS
-    
-    a = s.split(/}/)[0..-2].map do |entry|
-
-      raw_selector,raw_styles = entry.split(/{/,2)
-
-      h = raw_styles.split(/;/).inject({}) do |r, x| 
-        k, v = x.split(/:/,2).map(&:strip)
-        r.merge(k.to_sym => v)
-      end
-
-      [raw_selector.split(/,\s*/).map(&:strip), h]
-    end      
-
-    
-    # add each CSS style attribute to the element
-    # e.g. a = [[['line'],{stroke: 'green'}]]
-
-    a.each do |x|
-
-      selectors, style = x
-
-      selectors.each do |selector|
-
-        style.each do |k,v|
-          self.css(selector).each {|element| element.style[k] = v }
-        end
-
-      end
-    end
-    
-  end
-    
-  def scan_element(name, attributes=nil, *children)
-
-    return Rexle::CData.new(children.first) if name == '!['
-    return Rexle::Comment.new(children.first) if name == '!-'
-
-    type = {
+  def defined_elements()
+    {
       circle: Svgle::Circle,
       ellipse: Svgle::Ellipse,
       line: Svgle::Line,
@@ -310,37 +160,7 @@ class Svgle < Rexle
       path: Svgle::Path,
       rect: Svgle::Rect,
       text: Svgle::Text
-    }
-
-    element = type[name.to_sym].new(name, attributes: attributes, rexle: self)  
-
-    if children then
-
-      children.each do |x|
-        if x.is_a? Array then
-
-          element.add_element scan_element(*x)        
-        elsif x.is_a? String then
-
-          e = if x.is_a? String then
-
-            x
-          elsif x.name == '![' then
-
-            Rexle::CData.new(x)
-            
-          elsif x.name == '!-' then
-
-            Rexle::Comment.new(x)
-            
-          end
-
-          element.add_element e
-        end
-      end
-    end
-    
-    return element
+    }    
   end
 
 end
--- svgle.gemspec
+++ svgle.gemspec
@@ -1,11 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'svgle'
-  s.version = '0.3.1'
-  s.summary = 'Svgle (SVG + Rexle) generates an SVG Document Object Model from the given SVG. Suitable for accessing, or modifying SVG elements as objects.'
+  s.version = '0.4.0'
+  s.summary = 'Svgle (SVG + Domle) generates an SVG Document Object Model from the given SVG. Suitable for accessing, or modifying SVG elements as objects.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/svgle.rb']
-  s.add_runtime_dependency('rexle', '~> 1.3', '>=1.3.9')
-  s.add_runtime_dependency('rxfhelper', '~> 0.2', '>=0.2.3')
+  s.add_runtime_dependency('domle', '~> 0.1', '>=0.1.0')
   s.signing_key = '../privatekeys/svgle.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
